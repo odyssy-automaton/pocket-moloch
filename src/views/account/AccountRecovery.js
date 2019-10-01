217078724455b731e592141bcff7af4f37be5e83
oovg <office@vengist.com>
2019-10-01 16:27:01 -0700

    adds styles back to new-account-ux-revision

--- src/views/account/AccountRecovery.js
+++ src/views/account/AccountRecovery.js
@@ -3,9 +3,6 @@ import React, { useContext, useState, useEffect } from 'react';
 import { LoaderContext, CurrentUserContext } from '../../contexts/Store';
 import Loading from '../../components/shared/Loading';
 import './AccountRecovery.scss';
-import WhiteCheck from '../../assets/WhiteCheckSmall.svg';
-import EthLogo from '../../assets/Ethereum_logo_2014.svg';
-import PlusSign from '../../assets/+.svg';
 import Modal from '../../components/shared/Modal';
 import useModal from '../../components/shared/useModal';
 import useInterval from '../../utils/PollingUtil';
@@ -205,7 +202,12 @@ const AccountRecovery = ({ history }) => {
               Sign in on another device or browser and scan the QR Code there to
               give that device or browser access.
             </p>
-            <p>Or paste here</p>
+            <QrReader
+              delay={500}
+              onError={handleError}
+              onScan={handleScan}
+              style={{ width: '80%' }}
+            />
             <Formik
               initialValues={{
                 newAddr: '',
@@ -228,12 +230,13 @@ const AccountRecovery = ({ history }) => {
             >
               {({ isSubmitting }) => (
                 <Form className="Form">
+                  <p>Or Copy the link from the other device and paste here.</p>
                   <Field name="newAddr">
                     {({ field, form }) => (
                       <div
                         className={field.value ? 'Field HasValue' : 'Field '}
                       >
-                        <label>paste address</label>
+                        <label>Paste the Link</label>
                         <input type="text" {...field} />
                       </div>
                     )}
@@ -243,17 +246,11 @@ const AccountRecovery = ({ history }) => {
                     render={(msg) => <div className="Error">{msg}</div>}
                   />
                   <button type="submit" disabled={isSubmitting}>
-                    add
+                    Submit
                   </button>
                 </Form>
               )}
             </Formik>
-            <QrReader
-              delay={500}
-              onError={handleError}
-              onScan={handleScan}
-              style={{ width: '80%' }}
-            />
           </div>
         </Modal>
       )}
@@ -290,48 +287,77 @@ const AccountRecovery = ({ history }) => {
         </div>
       </Modal>
       {(loading || waitingSdk) && <Loading />}
-      <h2 className="Pad"> Recovery Options</h2>
-      <div className="RecoveryOption">
-        <p className="atSign">@</p>
-        <p>Email</p>
-        <img src={WhiteCheck} alt="white check" />
-      </div>
+      <h2 className="Pad">Recovery Options</h2>
+      <button className="Button--Input Email Verified">
+        <svg
+          className="Icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+          <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+        Email
+        <svg className="AddItem" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0zm0 0h24v24H0V0z"/><path d="M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>
+      </button>
 
       {accountDevices.map((item) => (
-        <div key={item.device.address} className="RecoveryOption">
-          <p className="atSign">@</p>
-          <p>
-            {Object.keys(parsedNamedDevices).find(
-              (key) => parsedNamedDevices[key] === item.device.address,
-            ) || item.device.address}
-          </p>
-          <img src={WhiteCheck} alt="white check" />
-        </div>
+        <button key={item.device.address} className={isThisDeviceAdded ? 'Button--Input Verified' : 'Button--Input'}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M17,1H7A2,2,0,0,0,5,3V21a2,2,0,0,0,2,2H17a2,2,0,0,0,2-2V3A2,2,0,0,0,17,1Zm0,18H7V5H17Z" />
+          </svg>
+          {Object.keys(parsedNamedDevices).find(
+            (key) => parsedNamedDevices[key] === item.device.address,
+          ) || item.device.address}
+          <svg className="AddItem" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0zm0 0h24v24H0V0z"/><path d="M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>
+        </button>
       ))}
-      <div
+      <button
         onClick={isThisDeviceAdded ? null : () => toggle('getreadQrCode')}
-        className="RecoveryOption"
-        style={{
-          backgroundColor: isThisDeviceAdded ? 'greenyellow' : 'whitesmoke',
-        }}
+        className={isThisDeviceAdded ? 'Button--Input Verified' : 'Button--Input'}
       >
-        <p className="atSign">@</p>
-        <p>{isThisDeviceAdded ? 'This device' : 'Add this device'}</p>
-        <img
-          src={isThisDeviceAdded ? WhiteCheck : PlusSign}
-          alt="device status"
-        />
-      </div>
-      <div className="AddDeviceList" onClick={() => setshowQrreader(true)}>
-        <img src={EthLogo} alt="eth logo" className="AddDeviceEthLogo" />
-        <p className="PinkText">Add another Device</p>
-        <img src={PlusSign} alt="device status" />
-      </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M17,1H7A2,2,0,0,0,5,3V21a2,2,0,0,0,2,2H17a2,2,0,0,0,2-2V3A2,2,0,0,0,17,1Zm0,18H7V5H17Z" />
+        </svg>
+        {isThisDeviceAdded ? 'This device' : 'Add this device'}
+        {isThisDeviceAdded ? (
+          <svg className="AddItem" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0zm0 0h24v24H0V0z"/><path d="M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="AddItem"
+          >
+            <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+            <path d="M0 0h24v24H0z" fill="none" />
+          </svg>
+        )}
+      </button>
+      <button className="Button--Input" onClick={() => setshowQrreader(true)}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M17,1H7A2,2,0,0,0,5,3V21a2,2,0,0,0,2,2H17a2,2,0,0,0,2-2V3A2,2,0,0,0,17,1Zm0,18H7V5H17Z" />
+        </svg>
+        Add another Device
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="AddItem"
+        >
+          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+          <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+      </button>
       {accountDevices.length === 0 ? (
-        <p>
+        <p><strong>
           You need to add at least one more device or browser with access to use
           as a recovery option.
-        </p>
+        </strong></p>
       ) : (
         <button onClick={() => history.push('/account')}>Continue</button>
       )}
