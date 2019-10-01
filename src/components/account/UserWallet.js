2692ffe4c13e3d92b2e3e154a805a2fa5626d0ad
Damjan Coric <damjan.coric@mail.com>
2019-10-01 23:43:14 +0200

    Add deposit modal

--- src/components/account/UserWallet.js
+++ src/components/account/UserWallet.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 
 import useModal from '../shared/useModal';
 import Modal from '../shared/Modal';
-
+import CopyToClipboard from 'react-copy-to-clipboard';
 import ConnectAccount from './ConnectAccount';
 
 import {
@@ -23,29 +23,45 @@ import ApproveWeth from './ApproveWeth';
 import RageQuit from './RageQuit';
 import DepositForm from './DepositForm';
 import TwoButtonModal from '../../components/shared/TwoButtonModal';
-
+import useInterval from '../../utils/PollingUtil';
 const UserWallet = ({history}) => {
+
+  const [delay, setDelay] = useState(null);
+  const [copied, setCopied] = useState(false);
   const [currentUser] = useContext(CurrentUserContext);
   const [loading] = useContext(LoaderContext);
   const [currentWallet] = useContext(CurrentWalletContext);
   const { isShowing, toggle } = useModal();
   const [waitingSdk, setWaitingSdk] =useState(true);
+
   useEffect(()=>{
     (async () => {
         if (currentUser && currentUser.sdk) {
-        
           const _accountDevices = await currentUser.sdk.getConnectedAccountDevices();
           setWaitingSdk(false);
+          console.log(currentUser.sdk.state.account.balance.real.toString()/1000000000000000000);
           if (!_accountDevices.items.some(item=> item.device.address === currentUser.sdk.state.deviceAddress)) {
             toggle('newDeviceDetectedModal')
           } 
           else if (_accountDevices.items.length<2) {
             toggle('addDeviceModal')
           }
+          else if (currentUser.sdk.state.account.state==='Created'&&((currentUser.sdk.state.account.balance.real.toString()/1000000000000000000)<0.001)){
+            toggle('depositModal')
+          }
         }
     })()
     // eslint-disable-next-line
   },[currentUser]);
+  const onCopy = () => {
+    setDelay(2500);
+    setCopied(true);
+  };
+
+  useInterval(() => {
+    setCopied(false);
+    setDelay(null);
+  }, delay);
   return (
     <>
       {(loading  || waitingSdk) && <Loading />}
@@ -66,6 +82,34 @@ const UserWallet = ({history}) => {
             text="You need to add at least one more recovery option"
             handleConfirm={()=>history.push('/account-recovery')}
           />
+          <Modal
+            isShowing={isShowing.depositModal}
+            hide={() => toggle('depositModal')}
+              >
+               {copied && (
+        <div className="Flash">
+          <p>Copied!</p>
+        </div>
+      )}
+            <h3>Deposit</h3>
+            <p>To complete setup of your account, please deposit 0.05 ETH to cover costs of deployment.</p>
+            <p>{currentUser.sdk.state.account.address}</p>
+            <CopyToClipboard onCopy={onCopy} text={currentUser.sdk.state.account.address}>
+                <button className="Address">
+                  Copy Link
+                  <svg
+                    className="IconRight"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0V0z" />
+                    <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4H8c-1.1 0-1.99.9-1.99 2L6 21c0 1.1.89 2 1.99 2H19c1.1 0 2-.9 2-2V11l-6-6zM8 21V7h6v5h5v9H8z" />
+                  </svg>
+                </button>
+              </CopyToClipboard>
+          </Modal>
         <div className="UserWallet">
           <UserBalance />
           <div className="Actions Pad">
